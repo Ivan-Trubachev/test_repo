c1fb6c6e0c5b54f1ac63a097b7f39c35a5c19e15
Ivan Trubachev <ivan.trubachev@unikie.com>
2024-05-20 12:28:32 +0400

    Order test

--- osf/default.nix
+++ osf/default.nix
@@ -29,7 +29,7 @@ in
       enable = true;
       description = "OSF service";
       wantedBy = [ "default.target" ];
-      after = [ "systemd-networkd-wait-online.service" ];
+      after = [ "systemd-logind.service" ];
       serviceConfig.ExecStart = "osf_control.sh setup /dev/nrf0 osf0 /dev/nrfdfu0";
       serviceConfig.ExecSearchPath ="/run/current-system/sw/bin";
       serviceConfig.Environment ="PATH=$PATH:/run/current-system/sw/bin";
