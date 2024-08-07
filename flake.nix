5f8b7a927bbc28cc5df83f78ba635fef97acf7b4
Ivan Trubachev <ivan.trubachev@unikie.com>
2024-08-07 17:47:13 +0400

    Added NXP driver for Azurewave CM358

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "newracom-flake";
 
   outputs = _: {
-      nrc_driver = import ./nrc_driver;
-      nrc_utils = import ./nrc_utils;
+      mwifiex = import ./mwifiex;
   };
 }
---
+++ mwifiex/default.nix
@@ -0,0 +1,31 @@
+# Copyright 2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.nxp.mwifiex-drv;
+  mwifiex-drv = config.boot.kernelPackages.callPackage ./packages/mwifiex {};
+in
+  with lib; {
+    options.nxp.mwifiex-drv = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          If enabled, NXP driver for AzureWave CM358 driver modules will be installed
+        '';
+      };
+    };
+    
+    config = mkIf cfg.enable {
+        # Export kernel modules and perform modprobe on boot with arguments
+        boot.extraModulePackages = [ mwifiex-drv ];
+        boot.kernelModules = [ "mlan" "moal"];
+        boot.extraModprobeConfig = ''
+          options moal sta_name=sta uap_name=sap max_vir_bss=1 cfg80211_wext=0xf cal_data_cfg=none fw_name=NXP8987/sdiouart8987_combo_v0.bin
+          '';
+    };
+  }
